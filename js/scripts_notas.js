ca4ce882603a09043ea718c4b7b2c7936e28ce8c
anadir nombre Jorge Torrejon <jtlopez@alumnos.upm.es>
2023-10-11 01:55:49 +0200

    comentarios

--- js/scripts_notas.js
+++ js/scripts_notas.js
@@ -2,44 +2,37 @@ const DATOS = [{ titulo: 'Ejemplo título', categoria: 'Categoría 1', contenido
     ,{titulo: 'Ejemplo título 2', categoria: 'Categoría 2', contenido: 'abcdef'}
     ,{titulo: 'Ejemplo título 3', categoria: 'Categoría 5', contenido: '123456789'}
     ,{titulo: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem', categoria: 'Categoría 1', contenido: 'asdfghj'}
-]
+] //cartas ya existentes
 
-const categorias = ["Categoría 1","Categoría 2","Categoría 3","Categoría 4","Categoría 5"];
-const colorCategorias = [];
 
-const nCategorias=[]
+
+const categorias = ["Categoría 1","Categoría 2","Categoría 3","Categoría 4","Categoría 5"]; //categorías ya existentes
+const colorCategorias = []; //colores de las categorias 
+
+const nCategorias=[] //numero de cartas por la categoría
 
 for (let i =0;i<categorias.length;i++){
     nCategorias.push(0)
 }
 
-let categoria_insertar = document.getElementById('categoria-insertar')
-let nota_insertar = document.getElementById('nota-insertar')
-
-function tituloEnUso(titulo) {
-    for (let i = 0; i < DATOS.length; i++) {
-        const tituloDatos = DATOS[i].titulo;
-
-        if (titulo === tituloDatos) {
-            return i;
-        }
-    }
-    return -1
-}
-
-
+//nueva nota
 function anadirNota(){
-    const cardDeck = document.getElementById("cards")
+    const cardDeck = document.getElementById("cards") //elemento en el que se quiere insertar la carta
+
+    //Campos a insertar
     const titulo = document.getElementById("titulo-insertar").value
     const categoria = document.getElementById("categoria-insertar").value
-    const contenido = document.getElementById("contenido-insertar").value
+    const contenido = document.getElementById("contenido-insertar").value 
 
-    if (!tituloEnUso(titulo) !== -1 && titulo !== "" && contenido !== ""){
+    //Los campos no pueden estar vacíos
+    if (titulo !== "" && contenido !== ""){
         DATOS.push({titulo, categoria, contenido})
     }
 
     cardDeck.innerHTML = "";
     i=0
+
+    //Se añade cada carta
     DATOS.forEach(function (dato){
         const card = document.createElement("div")
         card.className="card mr-5 ml-5 mb-5"
@@ -47,14 +40,16 @@ function anadirNota(){
         card.id = dato.titulo
         card.style = "max-width: 19rem; min-width: 15rem;  min-height: 15rem; max-height: 15rem"
         card.innerHTML += 
-            `<div class="d-flex justify-content-between align-items-center ">
+            `
+            <!--Card de la nota -->
+            <div class="d-flex justify-content-between align-items-center ">
                 <div class="mt-2 ml-2" style="max-width:83%;white-space: nowrap;overflow: hidden; text-overflow: ellipsis;"><h4>${dato.titulo}</h4></div>
                     <button class="btn" style="margin-right: 2%;" type="button" id="dropdownNota" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
                         <img src="mostrar-mas-boton-con-tres-puntos.png" height ="15" width="15" />
                     </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownNota">
                         <a class="dropdown-item" data-toggle="modal" data-target="#editarCard-${i}">Editar</a>
-                        <a class="dropdown-item" data-toggle="modal" data-target="#seguro">Eliminar</a>
+                        <a class="dropdown-item" data-toggle="modal" data-target="#seguro-${i}">Eliminar</a>
                     </div>
             </div>
             <div class = "categoria ml-2" style="background-color: ${colorCategorias[categorias.indexOf(dato.categoria)]}">${dato.categoria}</div>
@@ -65,7 +60,8 @@ function anadirNota(){
 
             
             
-
+            
+            <!--Popup de añadir nota-->
             <div class="modal fade" id="card-${i}" tabindex="-1" role="dialog" aria-labelledby="card-${i}Label" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -87,6 +83,11 @@ function anadirNota(){
                 </div>
             </div>
 
+
+
+
+
+            <!--Popup de editar nota -->
             <div class="modal fade" id="editarCard-${i}" tabindex="-1" role="dialog" aria-labelledby="editarCard-${i}Label" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -121,7 +122,11 @@ function anadirNota(){
                 </div>
             </div>
 
-            <div class="modal fade" id="seguro" tabindex="-1" role="dialog" aria-labelledby="seguroLabel" aria-hidden="true">
+
+
+
+            <!--Popup de eliminar nota -->
+            <div class="modal fade" id="seguro-${i}" tabindex="-1" role="dialog" aria-labelledby="seguroLabel-${i}" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                     <div class="modal-header">
@@ -139,61 +144,76 @@ function anadirNota(){
             </div>
             `
         cardDeck.appendChild(card);
+        //actualizar los demás elementos
         agregarCategoria(i);
         contarCategorias();
-        crearGraficas();
+        crearGraficas(); 
         document.getElementById("categoria-editarCard-"+i).value = dato.categoria
         i++
-        console.log(i)
-    })
+    });
     
 }
 
-function agregarCategoria(i) {
-    const inputNuevaCategoria = document.getElementById("categoriaNueva-insertar");
-        if (i == -1){
-            selectCategoria = document.getElementById("categoria-insertar");
-        }
-        else{
-            selectCategoria = document.getElementById("categoria-editarCard-"+i);
-        }
-        
-        if (inputNuevaCategoria.value !== "") {
-            categorias.push(inputNuevaCategoria.value);
-            nCategorias.push(0)
-        }
-        selectCategoria.innerHTML = "";
-        categorias.forEach(function (categoria) {
-            const option = document.createElement("option");
-            option.value = categoria;
-            option.textContent = categoria;
-            if (colorCategorias.length !== categorias.length){
-                colorCategorias.push(generarColorAleatorio())
-            }
-            selectCategoria.appendChild(option);
-        });
-    crearGraficas()
-    }
 
+//nueva categoría
+function agregarCategoria(i) {
+    const inputNuevaCategoria = document.getElementById("categoriaNueva-insertar"); //categoria a insertar
+
+    //en que elementos se actualiza la lista de categorías
+    if (i == -1){
+        selectCategoria = document.getElementById("categoria-insertar");
+    }
+    else{
+        selectCategoria = document.getElementById("categoria-editarCard-"+i);
+    }
+    
+    //se añade a la lista la categoría
+    if (inputNuevaCategoria.value !== "") {
+        categorias.push(inputNuevaCategoria.value);
+        nCategorias.push(0)
+    }
+    selectCategoria.innerHTML = "";
+
+    //se añade todas las categorías a los seleccionables correspondientes
+    categorias.forEach(function (categoria) {
+        const option = document.createElement("option");
+        option.value = categoria;
+        option.textContent = categoria;
+        if (colorCategorias.length !== categorias.length){
+            colorCategorias.push(generarColorAleatorio())
+        }
+        selectCategoria.appendChild(option);
+    });
+
+    //se actualizan los demás elementos
+    crearGraficas()
+}
+
+//editar nota
 function editarNota(i){
-    console.log(DATOS)
+    //elementos a cambiar
     const titulo = document.getElementById("titulo-editarCard-"+i).value
     const categoria = document.getElementById("categoria-editarCard-"+i).value
     const contenido = document.getElementById("contenido-editarCard-"+i).value
-    if (!tituloEnUso(titulo) !== -1 && titulo !== "" && contenido !== ""){
+
+    //actualización de los datos si no son vacíos
+    if (titulo !== "" && contenido !== ""){
         DATOS[i].titulo = titulo
         DATOS[i].categoria = categoria
         DATOS[i].contenido = contenido
     }
+
+    //actualización de los demás elementos
     anadirNota()
 }
 
+//eliminar nota
 function eliminarNota(i){
-    console.log(i)
-    DATOS.splice(i,1)
-    anadirNota()
+    DATOS.splice(i,1);
+    anadirNota();
 }
 
+//colores claros aleatorios
 function generarColorAleatorio() {
     const r = Math.floor(Math.random() * 100 + 155);
     const g = Math.floor(Math.random() * 100 + 155);
@@ -204,6 +224,7 @@ function generarColorAleatorio() {
     return colorHex;
 }
 
+//rellena nCategorias
 function contarCategorias(){
     for (let i =0;i<categorias.length;i++){
         nCategorias[i]=0
@@ -214,6 +235,7 @@ function contarCategorias(){
     });
 }
 
+//genera las gráficas
 function crearGraficas(){
     const $graficaBarras = document.querySelector("#graficaBarras");
     const $graficaCircular = document.querySelector("#graficaCircular");
@@ -264,6 +286,7 @@ function crearGraficas(){
     });
 }
 
+//funciones a realizar al cargar
 document.addEventListener("DOMContentLoaded", function () {
     agregarCategoria(-1);
     anadirNota();
@@ -271,11 +294,13 @@ document.addEventListener("DOMContentLoaded", function () {
     crearGraficas();
 });
 
+//resetear inputs al salir del popup
 $('#insertarFila').on('hidden.bs.modal', function(){
     document.getElementById("titulo-insertar").value = ""
     document.getElementById("contenido-insertar").value = ""
 })
 
+//resetear inputs al salir del popup
 $('#insertarCategoria').on('hidden.bs.modal', function(){
     document.getElementById("categoriaNueva-insertar").value = ""
 })
